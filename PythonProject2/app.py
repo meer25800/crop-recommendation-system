e24810993c7cb1c6096ae0e54e9504d0d37e310a
Aamir Hameed mir <30907975+meer25800@users.noreply.github.com>
2025-02-06 00:58:25 +0530

    Update app.py

--- PythonProject2/app.py
+++ PythonProject2/app.py
@@ -82,7 +82,7 @@ if st.sidebar.button("🌱 Recommend Crop"):
     st.sidebar.success(f"✅ Recommended Crop: **{prediction}**")
 
     # --- Visualization ---
-    fig, ax = plt.subplots(figsize=(5,5 ))
+    fig, ax = plt.subplots(figsize=(3,3 ))
     input_values = [N, P, K, temperature, humidity, ph, rainfall]
     input_labels = ["Nitrogen", "Phosphorus", "Potassium", "Temp (°C)", "Humidity (%)", "pH", "Rainfall (mm)"]
 
